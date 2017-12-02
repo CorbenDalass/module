26e1b7c588e80952e5085a6eba392f31157d5937
CorbenDalass <batimreysa@gmail.com>
2017-12-02 19:25:12 +0200

    Четвертая задача

--- src/Zadacha4/Chetvertaya.java
+++ src/Zadacha4/Chetvertaya.java
@@ -1,18 +1,33 @@
 package Zadacha4;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
-public class Chetvertaya<E> {
+public class Chetvertaya {
     public static void main(String[] args) throws FileNotFoundException {
         String LoginsAndPassword = FileUtils.read("src/Zadacha4/LoginsAndPassword.txt");
-        String[]massiv=LoginsAndPassword.split("\n");
-        for (int i=0;i<10;i++){
-            System.out.println(massiv[i]);
+        String[] massiv = LoginsAndPassword.split("\n");
+        String[] paroli = new String[massiv.length];
+        for (int i = 0; i < massiv.length; i++) {
+            paroli[i] = massiv[i].split(" ")[1];
         }
-
+        List<ObuchnueParolli> korobkaGdeParoli = new ArrayList<>();
+        for (int i = 0; i < paroli.length; i++) {
+            boolean sovpadPparol=false;
+            for (int x = 0; x < korobkaGdeParoli.size(); x++) {
+                if (paroli[i].equals(korobkaGdeParoli.get(x).getParol())) {
+                    korobkaGdeParoli.get(x).setSchetchikSovpadeniya(korobkaGdeParoli.get(x).getSchetchikSovpadeniya()+1);
+                    sovpadPparol=true;
+                    break;
+                }
+            }
+            if (!sovpadPparol){
+                korobkaGdeParoli.add(new ObuchnueParolli(paroli[i]));
+            }
+        }
+        Collections.sort(korobkaGdeParoli);
+        for (int i=0;i<10;i++){
+            System.out.println("pass - "+korobkaGdeParoli.get(i).getParol()+"   naideno "+korobkaGdeParoli.get(i).getSchetchikSovpadeniya());
+        }
     }
 }
--- src/Zadacha4/ObuchnueParolli.java
+++ src/Zadacha4/ObuchnueParolli.java
@@ -1,5 +1,28 @@
 package Zadacha4;
 
-public class ObuchnueParolli {
+public class ObuchnueParolli implements Comparable{
+    private int schetchikSovpadeniya=1;
+    private String parol;
 
+    public ObuchnueParolli(String parolb){
+        parol=parolb;
+    }
+
+    public String getParol() {
+        return parol;
+    }
+
+    public int getSchetchikSovpadeniya() {
+        return schetchikSovpadeniya;
+    }
+
+    public void setSchetchikSovpadeniya(int schetchikSovpadeniya) {
+        this.schetchikSovpadeniya = schetchikSovpadeniya;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return ((ObuchnueParolli)o).getSchetchikSovpadeniya() - this.schetchikSovpadeniya;
+    }
 }
+
